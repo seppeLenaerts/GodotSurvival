126b41935699a8b80d3fa771d024071c303eec99
Seppe <lenaertssep@gmail.com>
2025-06-08 13:51:54 +0200

    Add WASD movement

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -8,6 +8,11 @@ namespace Game.Scenes;
 public partial class Main : Node2D
 {
 
+	private const string MOVE_LEFT = "move_left";
+	private const string MOVE_RIGHT = "move_right";
+	private const string MOVE_UP = "move_up";
+	private const string MOVE_DOWN = "move_down";
+
 	private const int GRID_SIZE = 16;
 
 	[Export]
@@ -27,23 +32,28 @@ public partial class Main : Node2D
 
 	public override void _Input(InputEvent inputEvent)
 	{
+		if (inputEvent is InputEventKey key)
+		{
+			HandleInputKey(key);
+		}
+
 		if (inputEvent is InputEventMouse mouse)
 		{
 			if (mouse.ButtonMask == MouseButtonMask.Left)
 			{
-				HandleMoveAction(mouse);
+				HandleMoveActionClick(mouse);
 			}
 
 			if (mouse.ButtonMask == MouseButtonMask.Right)
 			{
-				HandleHarvestAction(mouse);
+				HandleHarvestAction();
 			}
 		}
 	}
 
-	private void HandleHarvestAction(InputEventMouse inputEventMouse)
+	private void HandleHarvestAction()
 	{
-		var gridPos = GetGridCell(inputEventMouse.GlobalPosition);
+		var gridPos = GetObjectGridCell(GetGlobalMousePosition());
 		var tile = objectLayer.GetCellTileData(gridPos);
 		if (tile != null && tile.HasCustomData("harvestable") && (bool)tile.GetCustomData("harvestable"))
 		{
@@ -57,23 +67,53 @@ public partial class Main : Node2D
 		}
 	}
 
-	private void HandleMoveAction(InputEventMouse inputEventMouse)
+	private void HandleInputKey(InputEventKey key)
 	{
-		var gridPos = GetGridCell(inputEventMouse.GlobalPosition);
+		if (key.IsAction(MOVE_LEFT))
+		{
+			HandleMoveCommand(GetPlayerGridCell() - new Vector2I(1, 0));
+		}
+		else if (key.IsAction(MOVE_RIGHT))
+		{
+			HandleMoveCommand(GetPlayerGridCell() - new Vector2I(-1, 0));
+		}
+		else if (key.IsAction(MOVE_UP))
+		{
+			HandleMoveCommand(GetPlayerGridCell() - new Vector2I(0, 1));
+		}
+		else if (key.IsAction(MOVE_DOWN))
+		{
+			HandleMoveCommand(GetPlayerGridCell() - new Vector2I(0, -1));
+		}
+	}
+
+	private void HandleMoveCommand(Vector2I targetPos)
+	{
+		player.SetVelocityTowards(targetPos * GRID_SIZE);
+	}
+
+	private void HandleMoveActionClick(InputEventMouse inputEventMouse)
+	{
+		var gridPos = GetObjectGridCell(GetGlobalMousePosition());
 		if (IsCellWalkable(inputEventMouse.GlobalPosition))
 		{
 			player.SetVelocityTowards(gridPos * GRID_SIZE);
 		}
 	}
 
-	private Vector2I GetGridCell(Vector2 pos)
+	private Vector2I GetObjectGridCell(Vector2 pos)
 	{
 		return objectLayer.LocalToMap(ToLocal(pos));
 	}
 
+	private Vector2I GetPlayerGridCell()
+	{
+		return GetObjectGridCell(player.GlobalPosition);
+	}
+
 	private bool IsPlayerInSurroundingCell(Vector2I gridCell, int radius)
 	{
-		var playerCell = GetGridCell(player.GlobalPosition);
+		var playerCell = GetPlayerGridCell();
 		var surroundingCells = new List<Vector2I>();
 		for (int x = -1 * radius; x <= 1 * radius; x++)
 		{
